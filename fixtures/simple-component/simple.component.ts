61c61711815d30b169697265eff224c7dfb2359d
George Kalpakas <kalpakas.g@gmail.com>
2019-09-11 17:08:18 +0300

    [lab-4.a] Feature: Show Angular template on hover

---
+++ fixtures/simple-component/simple.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'simple-component',
+  templateUrl: './simple.component.html',
+  styleUrls: ['./simple.component.css'],
+})
+export class SimpleComponent {
+  simple = true;
+}
