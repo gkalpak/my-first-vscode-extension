f97d9923574e7c36c50164d869e4f443a1b061c9
George Kalpakas <kalpakas.g@gmail.com>
2019-09-11 17:48:11 +0300

    [lab-4] Feature: Show Angular template on hover

--- src/template-url-intellisense-provider.ts
+++ src/template-url-intellisense-provider.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 /**
@@ -15,7 +17,33 @@ export class TemplateUrlIntellisenseProvider implements vscode.HoverProvider {
       document: vscode.TextDocument,
       position: vscode.Position,
     ): vscode.ProviderResult<vscode.Hover> {
-    // TODO: Implement the actual logic.
-    return new vscode.Hover('Not implemented.');
+    // Get the line being hovered.
+    const line = document.lineAt(position);
+
+    // Check whether the line is a `templateUrl` line of the `@Component()` decorator and extract
+    // the template URL.
+    // (This is not a perfect RegExp, but is good enough for demonstration purposes.)
+    const templateMatch = /templateUrl:\s+["'](.+)["']/.exec(line.text);
+
+    if (templateMatch === null) {
+      // No match. We cannot provide a hover.
+      return null;
+    }
+
+    // Resolve the path to the template HTML file.
+    const relativeTemplatePath = templateMatch[1];
+    const absoluteComponentDir = path.dirname(document.fileName);
+    const absoluteTemplatePath = path.resolve(absoluteComponentDir, relativeTemplatePath);
+
+    if (!fs.existsSync(absoluteTemplatePath)) {
+      // The template file does not exist. We cannot provide a hover.
+      return null;
+    }
+
+    // Read the template HTML content.
+    const templateCode = fs.readFileSync(absoluteTemplatePath, 'utf8');
+
+    // Return a `vscode.Hover` with the content of the component template formatted as HTML.
+    return new vscode.Hover({language: 'html', value: templateCode});
   }
 }
