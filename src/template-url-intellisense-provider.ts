8ce16fe3ee4524725fa056299ebaf4837d0425d7
George Kalpakas <kalpakas.g@gmail.com>
2019-09-12 10:46:28 +0300

    [lab-5] Feature: Peek/Go to Angular template definition

--- src/template-url-intellisense-provider.ts
+++ src/template-url-intellisense-provider.ts
@@ -18,8 +18,20 @@ export class TemplateUrlIntellisenseProvider
       document: vscode.TextDocument,
       position: vscode.Position,
     ): vscode.ProviderResult<vscode.Definition> {
-    // TODO: Implement the actual logic.
-    return null;
+    // Get the template HTML file path.
+    const templateFilePath = this.getTemplateFilePath(document, position);
+
+    if (templateFilePath === null) {
+      // No template file available. We cannot provide a definition.
+      return null;
+    }
+
+    // Return a `vscode.Definition` (i.e. a single `vscode.Location`) matching the whole template
+    // HTML file.
+    const definitionUri = vscode.Uri.file(templateFilePath);
+    const definitionRange = new vscode.Range(0, 0, Infinity, Infinity);
+
+    return new vscode.Location(definitionUri, definitionRange);
   }
 
   /**
@@ -33,7 +45,32 @@ export class TemplateUrlIntellisenseProvider
       document: vscode.TextDocument,
       position: vscode.Position,
     ): vscode.ProviderResult<vscode.Hover> {
-    // Get the line being hovered.
+    // Get the template HTML file path.
+    const templateFilePath = this.getTemplateFilePath(document, position);
+
+    if (templateFilePath === null) {
+      // No template file available. We cannot provide a hover.
+      return null;
+    }
+
+    // Read the template HTML content.
+    const templateCode = fs.readFileSync(templateFilePath, 'utf8');
+
+    // Return a `vscode.Hover` with the content of the component template formatted as HTML.
+    return new vscode.Hover({language: 'html', value: templateCode});
+  }
+
+  /**
+   * Get the template HTML file path, if the specified document/position correspond to a
+   * `templateUrl` line in an Angular `@Component()` decorator.
+   *
+   * @param document The document in which the command was invoked.
+   * @param position The position at which the command was invoked.
+   * @return The template file path or `null` if the specified document/position do not correspond
+   *     to a `templateUrl` line or the template file does not exist.
+   */
+  getTemplateFilePath(document: vscode.TextDocument, position: vscode.Position): string | null {
+    // Get the target line.
     const line = document.lineAt(position);
 
     // Check whether the line is a `templateUrl` line of the `@Component()` decorator and extract
@@ -42,7 +79,7 @@ export class TemplateUrlIntellisenseProvider
     const templateMatch = /templateUrl:\s+["'](.+)["']/.exec(line.text);
 
     if (templateMatch === null) {
-      // No match. We cannot provide a hover.
+      // No match. We should not return a file path.
       return null;
     }
 
@@ -52,14 +89,10 @@ export class TemplateUrlIntellisenseProvider
     const absoluteTemplatePath = path.resolve(absoluteComponentDir, relativeTemplatePath);
 
     if (!fs.existsSync(absoluteTemplatePath)) {
-      // The template file does not exist. We cannot provide a hover.
+      // The template file does not exist. We should not return a file path.
       return null;
     }
 
-    // Read the template HTML content.
-    const templateCode = fs.readFileSync(absoluteTemplatePath, 'utf8');
-
-    // Return a `vscode.Hover` with the content of the component template formatted as HTML.
-    return new vscode.Hover({language: 'html', value: templateCode});
+    return absoluteTemplatePath;
   }
 }
