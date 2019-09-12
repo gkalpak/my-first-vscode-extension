f28916b62bd906e4c8216548d8ffb2e93038f7ec
George Kalpakas <kalpakas.g@gmail.com>
2019-09-12 10:12:24 +0300

    [lab-5.a] Feature: Peek/Go to Angular template definition

--- src/template-url-intellisense-provider.ts
+++ src/template-url-intellisense-provider.ts
@@ -5,7 +5,23 @@ import * as vscode from 'vscode';
 /**
  * A class that can provide intellisense for Angular component `templateUrl`.
  */
-export class TemplateUrlIntellisenseProvider implements vscode.HoverProvider {
+export class TemplateUrlIntellisenseProvider
+    implements vscode.DefinitionProvider, vscode.HoverProvider {
+  /**
+   * Provide the definition of the symbol at the given position and document.
+   *
+   * @param document The document in which the command was invoked.
+   * @param position The position at which the command was invoked.
+   * @return A definition. The lack of a result can be signaled by returning `undefined` or `null`.
+   */
+  provideDefinition(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+    ): vscode.ProviderResult<vscode.Definition> {
+    // TODO: Implement the actual logic.
+    return null;
+  }
+
   /**
    * Provide a hover for the given position and document.
    *
