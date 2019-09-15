7c2d94610ea9f972bc07260571f379f40d54fe41
George Kalpakas <kalpakas.g@gmail.com>
2019-09-15 16:36:18 +0300

    [lab-7.c] Unit testing

--- src/test/unit/vscode.mock.ts
+++ src/test/unit/vscode.mock.ts
@@ -1,3 +1,18 @@
 // Exports
 export const mockVscode = {
+  commands: {
+    registerCommand: noop,
+  },
+  languages: {
+    registerDefinitionProvider: noop,
+    registerHoverProvider: noop,
+  },
+  window: {
+    showInformationMessage: noop,
+  },
 };
+
+// Helpers
+function noop(): void {
+  return;
+}
