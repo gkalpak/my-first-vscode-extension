c200762dff8e71e8881608e7d62b4135fe66e7a8
George Kalpakas <kalpakas.g@gmail.com>
2019-09-15 16:36:18 +0300

    [lab-7.c] Unit testing

--- src/test/unit/extension.test.ts
+++ src/test/unit/extension.test.ts
@@ -1,7 +1,33 @@
 import * as assert from 'assert';
+import {afterEach, beforeEach} from 'mocha';
+import * as sinon from 'sinon';
+import * as vscode from 'vscode';
+import {activate} from '../../extension';
 
 suite('Extension', () => {
-  test('should have unit tests', () => {
-    assert.strictEqual(false, true);
+  let sandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  suite('activate()', () => {
+    let mockContext: vscode.ExtensionContext;
+
+    beforeEach(() => {
+      mockContext = {subscriptions: []} as any;
+      sandbox.stub(console, 'log');
+    });
+
+    test('registers an `extension.helloWorld` command', () => {
+      const spy = sandbox.stub(vscode.commands, 'registerCommand');
+      activate(mockContext);
+
+      assert(spy.calledWith('extension.helloWorld', sinon.match.func));
+    });
   });
 });
